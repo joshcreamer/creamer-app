a9630c3d6ee7b892da49cbc9cf21a695e89f4773
josh <josh_creamer@yahoo.com>
2011-02-21 00:18:10 -0600

    initial commit to upload heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreamerApp::Application.config.secret_token = '5913909d4f53a2b20b944c40d6e6dea7eb95d6819898cfbbb8de6fe7e416828d931bb9f1afafc9dd3e63382f6cc5db627d140a1c4a1fdf86191489806407e589'
